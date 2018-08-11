f85550fb3dcd1ab57d08642db79082818e59d494
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2018-08-11 23:26:39 +0700

    Master
    Original files of WeimengCMS

---
+++ V4.176.396/module/novel/type.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * 小说列表页
+ *
+ * @version        $Id: type.php 2015年11月08日 14:09  weimeng
+ * @package        WMCMS
+ * @copyright      Copyright (c) 2015 WeiMengCMS, Inc.
+ * @link           http://www.weimengcms.com
+ * @uptime		   2016年1月7日 15:47 weimeng
+ *
+ */
+$ClassArr = array('page');
+//引入模块公共文件
+require_once 'novel.common.php';
+
+
+//当前页面的参数检测
+$tid = str::IsEmpty( Get('tid') , $lang['novel']['par']['tid_no'] );
+$page = str::Page( Get('page') );
+
+//参数验证
+$where = novel::GetPar( 'type' , $tid);
+
+//获得页面的标题等信息
+$data = novel::GetData( 'type' , $where , $lang['system']['type']['no'] );
+$data['title'] = @$data[0]['type_title'];
+$data['key'] = @$data[0]['type_key'];
+$data['desc'] = @$data[0]['type_desc'];
+
+C('page' ,  array(
+	'pagetype'=>'novel_type' ,
+	'data'=>$data[0],
+	'tempid'=>'type_tempid' ,
+	'dtemp'=>'novel/type.html',
+	'label'=>'novellabel',
+	'label_fun'=>'TypeLabel',
+	'tid'=>$data[0]['type_id'],
+	'page'=>$page,
+	'listurl'=>tpl::url('novel_type',array('tid'=>$tid,'tpinyin'=>$data[0]['type_pinyin'],)),
+));
+
+//设置seo信息
+tpl::GetSeo();
+
+//创建模版并且输出
+$tpl=new tpl();
+//分类筛选模块
+IncModule('retrieval');
+$tpl->display();
+?>
---
+++ V4.176.396/wmcms/inc/imgcode.php
@@ -0,0 +1,5 @@
+<?php
+$C['module']['inc']['class']=array('img','str');
+require_once 'common.inc.php';
+echo Img::ImgCode(4);
+?>
