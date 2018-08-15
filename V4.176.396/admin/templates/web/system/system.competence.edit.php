933ce1b28f539c7e0a7d2c8010ad4a2b5745b35b
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2018-08-15 14:10:38 +0700

    Translated

--- V4.176.396/admin/templates/web/system/system.competence.edit.php
+++ V4.176.396/admin/templates/web/system/system.competence.edit.php
@@ -2,20 +2,20 @@
 <form action="index.php?a=yes&c=system.competence.competence&t=<?php echo $type;?>" data-reload="false" data-toggle="ajaxform" method="post" <?php if($d) { echo 'data-callback="'.$cFun.'"';}?>>
 <input name="id" type="hidden" class="input-text" value="<?php echo C('comp_id',null,'data');?>">
 <table class="table table-border table-bordered table-bg table-sort">
-	<th colspan="2" style="padding-left:10px;"><b>管理员可用权限选择</b></th></tr>
+	<th colspan="2" style="padding-left:10px;"><b>ผู้ดูแลสามารถเลือกสิทธิ์ได้</b></th></tr>
     <tr>
-      <td valign="top" width="200">权限名字：</td>
+      <td valign="top" width="200">ชื่อสิทธิ์ : </td>
       <td valign="top">
       	&nbsp;<input name="name" type="text" class="input-text" value="<?php echo C('comp_name',null,'data');?>">
       </td>
 	</tr>
 	<tr>
-    <td valign="top">管理站点：</td>
+    <td valign="top">จัดการเว็บไซต์ : </td>
     <td valign="top">
     <input id="<?php echo $cFun.$type;?>site" name="site" type="hidden" value="<?php echo C('comp_site',null,'data');?>">
     <input type="text" id="<?php echo $cFun.$type;?>site_ztree_input" data-toggle="selectztree" data-tree="#<?php echo $cFun.$type;?>site_ztree_select" readonly value="<?php echo C('comp_site_name',null,'data');?>">
     <ul id="<?php echo $cFun.$type;?>site_ztree_select" data-iptid="<?php echo $cFun.$type;?>site" class="ztree hide" data-toggle="ztree" data-expand-all="false" data-check-enable="true" data-chk-style="checkbox" data-radio-type="all" data-on-check="<?php echo $cFun.$type;?>S_NodeCheck" data-on-click="<?php echo $cFun.$type;?>S_NodeClick">
-     <?php 
+     <?php
      	$siteComp = explode(',', C('comp_site',null,'data'));
 	    foreach ($siteList as $k=>$v)
 	    {
@@ -30,12 +30,12 @@
    	</ul>
 	</tr>
 	<tr>
-    <td valign="top">可用权限选择：</td>
+    <td valign="top">สิทธิ์ที่ใช้ได้ : </td>
     <td valign="top">
     <input id="<?php echo $cFun.$type;?>comp" name="comp" type="hidden" value="<?php echo C('comp_content',null,'data');?>">
     <input type="text" id="<?php echo $cFun.$type;?>_ztree_input" data-toggle="selectztree" data-tree="#<?php echo $cFun.$type;?>_ztree_select" readonly value="<?php echo C('comp_content_name',null,'data');?>">
     <ul id="<?php echo $cFun.$type;?>_ztree_select" data-iptid="<?php echo $cFun.$type;?>comp" class="ztree hide" data-toggle="ztree" data-expand-all="false" data-check-enable="true" data-chk-style="checkbox" data-radio-type="all" data-on-check="<?php echo $cFun.$type;?>S_NodeCheck" data-on-click="<?php echo $cFun.$type;?>S_NodeClick">
-     <?php 
+     <?php
      	$checkedComp = explode(',', C('comp_content',null,'data'));
 	    foreach ($menuArr as $k=>$v)
 	    {
@@ -57,8 +57,8 @@
 
 <div class="bjui-pageFooter">
     <ul>
-        <li><button type="button" class="btn-close" data-icon="close">取消</button></li>
-        <li><button type="submit" class="btn-green" data-icon="save">保存</button></li>
+        <li><button type="button" class="btn-close" data-icon="close">ยกเลิก</button></li>
+        <li><button type="submit" class="btn-green" data-icon="save">จัดเก็บ</button></li>
     </ul>
 </div>
 
@@ -75,7 +75,7 @@ function <?php echo $cFun.$type;?>S_NodeCheck(e, treeId, treeNode) {
   var zTree = $.fn.zTree.getZTreeObj(treeId),
       nodes = zTree.getCheckedNodes(true)
   var ids = '', names = ''
-  
+
   for (var i = 0; i < nodes.length; i++) {
       ids   += ','+ nodes[i].id
       names += ','+ nodes[i].name
@@ -83,9 +83,9 @@ function <?php echo $cFun.$type;?>S_NodeCheck(e, treeId, treeNode) {
   if (ids.length > 0) {
       ids = ids.substr(1), names = names.substr(1)
   }
-  
+
   var $from = $('#'+ treeId).data('fromObj')
-  
+
   $("#"+$('#'+ treeId).data('iptid')).val(ids);
   if ($from && $from.length) $from.val(names)
 }
