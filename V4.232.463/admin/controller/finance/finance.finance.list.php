b53f3a9421e5391349c78f27276b3de4fd0d7daf
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2018-11-21 21:53:24 +0700

    V4.232.463
    Master files

---
+++ V4.232.463/admin/controller/finance/finance.finance.list.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* 资金变更记录控制器文件
+*
+* @version        $Id: finance.finance.list.php 2017年4月3日 14:24  weimeng
+* @package        WMCMS
+* @copyright      Copyright (c) 2015 WeiMengCMS, Inc.
+* @link           http://www.weimengcms.com
+*
+*/
+$config = GetModuleConfig('user');
+$logMod = NewModel('user.finance_log');
+
+//接受post数据
+$name = Request('name');
+
+//获取列表条件
+$where['table'] = '@user_finance_log';
+$where['field'] = '@user_finance_log.*,user_nickname';
+$where['left']['@user_user'] = 'log_user_id=user_id';
+if( $orderField == '' )
+{
+	$where['order'] = 'log_id desc';
+}
+
+
+//数据条数
+$total = wmsql::GetCount($where , 'log_id');
+
+//当前页的数据
+$where = array_merge($where , GetListWhere($where));
+$dataArr = wmsql::GetAll($where);
+?>
---
+++ V4.232.463/wmcms/action/user/checkemail.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* 邮箱地址检查请求处理
+*
+* @version        $Id: chaeckeamil.php 2016年5月28日 17:37  weimeng
+* @package        WMCMS
+* @copyright      Copyright (c) 2015 WeiMengCMS, Inc.
+* @link           http://www.weimengcms.com
+*
+*/
+$email = str::IsEmpty( Post('email') , $lang['user']['email_no'] );
+//邮件是否正确
+str::CheckEmail( $email , $lang['user']['email_err'] );
+//检查邮箱是否被注册
+user::CheckEmail( $email , $lang['user']['email_exist'] );
+
+ReturnData($lang['user']['email_success'], $ajax , 200);
+?>
