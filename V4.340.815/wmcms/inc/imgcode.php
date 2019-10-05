96438c461c570e3c24d169eb96d40805ca8dc6a9
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2019-10-06 00:04:44 +0700

    Remove previous version

--- V4.340.815/wmcms/inc/imgcode.php
+++
@@ -1,9 +0,0 @@
-<?php
-$C['module']['inc']['class']=array('img','str');
-require_once 'common.inc.php';
-if( Request('session_id') != '' )
-{
-	SetSessionId(Request('session_id'));
-}
-echo Img::ImgCode(4);
-?>
