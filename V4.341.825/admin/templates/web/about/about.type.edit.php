a6710dbc5a60253b832bbe663eba3d1d6f5e05d8
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2019-10-12 21:26:08 +0700

    Translated about admin template
    Translated about admin template to Thai

--- V4.341.825/admin/templates/web/about/about.type.edit.php
+++ V4.341.825/admin/templates/web/about/about.type.edit.php
@@ -2,24 +2,24 @@
     <form action="index.php?a=yes&c=about.type&t=<?php echo $type;?>" data-reload="false" data-toggle="validate" method="post" <?php if($d) { echo 'data-callback="'.$cFun.'"';}?>>
 	<input name="type_id" id="<?php echo $cFun.$type;?>type_id" type="hidden" class="input-text" value="<?php echo $id;?>">
 		<fieldset>
-			<legend>信息分类编辑</legend>
+			<legend>แก้ไขหมวดหมู่</legend>
             <ul class="nav nav-tabs" role="tablist">
-            	<li class="active"><a href="#<?php echo $cFun.$type;?>base" role="tab" data-toggle="tab">基本设置</a></li>
-            	<li><a href="#<?php echo $cFun.$type;?>seo" role="tab" data-toggle="tab">SEO设置</a></li>
-            	<li><a href="#<?php echo $cFun.$type;?>templates" id="<?php echo $cFun.$type;?>templates_tab" role="tab" data-toggle="tab">模版设置</a></li>
-            	<li><a href="#<?php echo $cFun.$type;?>html" role="tab" data-toggle="tab">HTML设置</a></li>
-            	<li><a href="#<?php echo $cFun.$type;?>field" id="<?php echo $cFun.$type;?>field_tab" role="tab" data-toggle="tab">自定义字段</a></li>
+            	<li class="active"><a href="#<?php echo $cFun.$type;?>base" role="tab" data-toggle="tab">ตั้งค่าทั่วไป</a></li>
+            	<li><a href="#<?php echo $cFun.$type;?>seo" role="tab" data-toggle="tab">ตั้งค่า SEO</a></li>
+            	<li><a href="#<?php echo $cFun.$type;?>templates" id="<?php echo $cFun.$type;?>templates_tab" role="tab" data-toggle="tab">ตั้งค่าเทมเพลต</a></li>
+            	<li><a href="#<?php echo $cFun.$type;?>html" role="tab" data-toggle="tab">ตั้งค่า HTML</a></li>
+            	<li><a href="#<?php echo $cFun.$type;?>field" id="<?php echo $cFun.$type;?>field_tab" role="tab" data-toggle="tab">ฟิลด์ที่กำหนดเอง</a></li>
             </ul>
             <div class="tab-content">
             	<div class="tab-pane fade active in" id="<?php echo $cFun.$type;?>base">
 					<table class="table table-border table-bordered table-bg table-sort">
 					    <tr>
-						  <td valign="top" width="150"><b>所属分类：</b></td>
+						  <td valign="top" width="150"><b>หมวดหมู่ : </b></td>
 					      <td valign="top">
 							<input id="<?php echo $cFun.$type;?>type_pid" name="type[type_topid]" type="hidden" value="<?php echo C('type_topid',null,'data');?>">
 					      	<input type="text" id="<?php echo $cFun.$type;?>_ztree_input" data-toggle="selectztree" data-tree="#<?php echo $cFun.$type;?>_ztree_select" readonly value="<?php echo C('name',null,'top');?>">
                             <ul id="<?php echo $cFun.$type;?>_ztree_select" class="ztree hide" data-toggle="ztree" data-expand-all="true" data-check-enable="true" data-chk-style="radio" data-radio-type="all" data-on-check="<?php echo $cFun.$type;?>S_NodeCheck" data-on-click="<?php echo $cFun.$type;?>S_NodeClick">
-                                <li data-id="0">顶级分类</li>
+                                <li data-id="0">หมวดหมู่สูงสุด</li>
                                 <?php 
                                 if( $typeArr )
                                 {
@@ -34,23 +34,23 @@
 					      </td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>分类名字：</b></td>
-					      <td valign="top"><input data-rule="分类名字:required;" name="type[type_name]" type="text" class="input-text" value="<?php echo C('type_name',null,'data');?>"></td>
+					      <td valign="top"><b>ชื่อ : </b></td>
+					      <td valign="top"><input data-rule="ชื่อหมวดหมู่:required;" name="type[type_name]" type="text" class="input-text" value="<?php echo C('type_name',null,'data');?>"></td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>分类简称：</b></td>
+					      <td valign="top"><b>ชื่อย่อ : </b></td>
 					      <td valign="top"><input name="type[type_cname]" type="text" class="input-text" value="<?php echo C('type_cname',null,'data');?>"></td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>分类拼音：</b></td>
+					      <td valign="top"><b>พินอิน : </b></td>
 					      <td valign="top"><input name="type[type_pinyin]" type="text" class="input-text" value="<?php echo C('type_pinyin',null,'data');?>"></td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>分类排序：</b></td>
+					      <td valign="top"><b>ลำดับ : </b></td>
 					      <td valign="top"><input data-rule="required;number;" name="type[type_order]" type="text" class="input-text" value="<?php echo C('type_order',null,'data');?>"></td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>分类简介：</b></td>
+					      <td valign="top"><b>คำอธิบาย : </b></td>
 					      <td valign="top"><textarea name="type[type_info]" class="form-control" cols="40" rows="6"><?php echo C('type_info',null,'data');?></textarea></td>
 						</tr>
 					</table>
@@ -59,15 +59,15 @@
 				<div class="tab-pane fade" id="<?php echo $cFun.$type;?>seo">
 					<table class="table table-border table-bordered table-bg table-sort">
 					    <tr>
-					      <td valign="top" width="150"><b>自定义标题：</b></td>
+					      <td valign="top" width="150"><b>ชื่อ : </b></td>
 					      <td valign="top"><input name="type[type_title]" type="text" class="input-text" size="40" value="<?php echo C('type_title',null,'data');?>"></td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>自定义关键字：</b></td>
+					      <td valign="top"><b>คำหลัก : </b></td>
 					      <td valign="top"><input name="type[type_key]" type="text" class="input-text" size="40" value="<?php echo C('type_key',null,'data');?>"></td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>自定义描述：</b></td>
+					      <td valign="top"><b>คำอธิบาย : </b></td>
 					      <td valign="top"><input name="type[type_desc]" type="text" class="input-text" size="40" value="<?php echo C('type_desc',null,'data');?>"></td>
 						</tr>
 					</table>
@@ -76,23 +76,23 @@
 				<div class="tab-pane fade" id="<?php echo $cFun.$type;?>templates">
 					<table class="table table-border table-bordered table-bg table-sort">
 					    <tr>
-					      <td valign="top" width="150"><b>信息列表页模版：</b></td>
+					      <td valign="top" width="150"><b>เทมเพลตหน้ารายการที่ใช้ : </b></td>
 					      <td valign="top">
 					      	<input id="type_tempid" name="type[type_tempid]" type="hidden" value="<?php echo C('type_tempid',null,'data');?>">
-					      	<input id="temp_name" name="temp[temp_name]" type="text" value="<?php echo C('name',null,'temp');?>" data-toggle="lookup" data-url="index.php?c=system.templates.lookup&module=about&page=list&name=name&tid=tempid" data-title="选择当前信息分类列表页模版" size="15" data-width="700" data-height="500">
-					      	<a class="btn btn-default" href="index.php?c=system.templates.edit&t=add&module=about&page=list&name=name&tid=tempid" data-toggle="dialog" data-title="上传新的信息列表页模版" data-width="540" data-height="450" ><i class="fa fa-cloud-upload">&nbsp;</i>上传模板</a>
+					      	<input id="temp_name" name="temp[temp_name]" type="text" value="<?php echo C('name',null,'temp');?>" data-toggle="lookup" data-url="index.php?c=system.templates.lookup&module=about&page=list&name=name&tid=tempid" data-title="เลือกเทมเพลตที่จะใช้กับหน้านี้" size="15" data-width="700" data-height="500">
+					      	<a class="btn btn-default" href="index.php?c=system.templates.edit&t=add&module=about&page=list&name=name&tid=tempid" data-toggle="dialog" data-title="อัปโหลดเทมเพลตใหม่" data-width="540" data-height="450" ><i class="fa fa-cloud-upload">&nbsp;</i>อัปโหลดเทมเพลต</a>
 					      </td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>信息内容页模版：</b></td>
+					      <td valign="top"><b>เทมเพลตหน้าเนื้อหาที่ใช้ : </b></td>
 					      <td valign="top">
 					      	<input id="type_ctempid" name="type[type_ctempid]" type="hidden" value="<?php echo C('type_ctempid',null,'data');?>">
-					      	<input id="temp_cname" name="temp[temp_cname]" type="text" value="<?php echo C('cname',null,'temp');?>" data-toggle="lookup" data-url="index.php?c=system.templates.lookup&module=about&page=content&name=cname&tid=ctempid" data-title="选择当前信息分类内容页模版" size="15" data-width="700" data-height="500">
-					      	<a class="btn btn-default" href="index.php?c=system.templates.edit&t=add&module=about&page=content&name=cname&tid=ctempid" data-toggle="dialog" data-title="上传新的信息内容页模版" data-width="540" data-height="450" ><i class="fa fa-cloud-upload">&nbsp;</i>上传模板</a>
+					      	<input id="temp_cname" name="temp[temp_cname]" type="text" value="<?php echo C('cname',null,'temp');?>" data-toggle="lookup" data-url="index.php?c=system.templates.lookup&module=about&page=content&name=cname&tid=ctempid" data-title="เลือกเทมเพลตที่จะใช้กับหน้านี้" size="15" data-width="700" data-height="500">
+					      	<a class="btn btn-default" href="index.php?c=system.templates.edit&t=add&module=about&page=content&name=cname&tid=ctempid" data-toggle="dialog" data-title="อัปโหลดเทมเพลตใหม่" data-width="540" data-height="450" ><i class="fa fa-cloud-upload">&nbsp;</i>อัปโหลดเทมเพลต</a>
 					      </td>
 						</tr>
 					    <tr>
-					      <td valign="top" colspan="2">不指定模版，将会使用当前应用的主题的自带模版！</td>
+					      <td valign="top" colspan="2">หากไม่ได้ระบุเทมเพลต ระบบจะใช้เทมเพลตที่ใช้ในปัจจุบัน!</td>
 						</tr>
 					</table>
 				</div>
@@ -100,11 +100,11 @@
 				<div class="tab-pane fade" id="<?php echo $cFun.$type;?>html">
 					<table class="table table-border table-bordered table-bg table-sort">
 					    <tr>
-					      <td valign="top" width="150"><b>列表页静态路径：</b></td>
+					      <td valign="top" width="150"><b>เส้นทางหน้ารายการแบบคงที่ : </b></td>
 					      <td valign="top"><input name="html[list]" value="<?php echo C('list',null,'html');?>" type="text" class="input-text" size="40"></td>
 						</tr>
 					    <tr>
-					      <td valign="top"><b>内容页静态路径：</b></td>
+					      <td valign="top"><b>เส้นทางหน้าเนื้อหาแบบคงที่ : </b></td>
 					      <td valign="top"><input name="html[content]" value="<?php echo C('content',null,'html');?>" type="text" class="input-text" size="40"></td>
 						</tr>
 					</table>
@@ -120,8 +120,8 @@
 
 <div class="bjui-pageFooter">
     <ul>
-        <li><button type="button" class="btn-close" data-icon="close">取消</button></li>
-        <li><button type="submit" class="btn-green" data-icon="save">保存</button></li>
+        <li><button type="button" class="btn-close" data-icon="close">ยกเลิก</button></li>
+        <li><button type="submit" class="btn-green" data-icon="save">จัดเก็บ</button></li>
     </ul>
 </div>
 
@@ -173,7 +173,7 @@ function <?php echo $cFun.$type;?>getfield(json){
 			      '<td valign="top">'+option[i]['form']+'</td></tr>';
 		}
 	}else{
-		html ='<tr><td valign="top"><b>请先添加自定义字段</b></td></tr>';
+		html ='<tr><td valign="top"><b>โปรดเพิ่มฟิลด์ที่กำหนดเองก่อน!</b></td></tr>';
 	}
 	$("#<?php echo $cFun.$type;?>field table").html(html);
 	$("#<?php echo $cFun.$type;?>field table").trigger('bjui.initUI');
@@ -200,7 +200,7 @@ $(document).ready(function(){
 		var pid = $("#<?php echo $cFun.$type;?>type_pid").val();
 
 		if( pid == '' ){
-			$(this).alertmsg('error', '对不起，请先选择分类');
+			$(this).alertmsg('error', 'ขออภัย! โปรดเลือกหมวดหมู่ก่อน');
 			return false;
 		}else{
 			if( '<?php echo C('type_topid',null,'data');?>' != pid){
