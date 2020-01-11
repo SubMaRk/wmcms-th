b4141e64e9bd5c5e64b2312f2dca96baaf43e2dd
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2020-01-11 20:46:46 +0700

    Clean Branch

--- V4.341.825/install/lock.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('WMINC'))die();?>
-<div class="main cc">
-	<div class="success_tip error_tip" style="margin-bottom: 30px;">
-	<p>คุณได้ติดตั้ง WMCMS ไปแล้ว หากคุณต้องการติดตั้งใหม่ โปรดลบไฟล์ (/wmcms/config/install.lock.txt) แล้วติดตั้งใหม่</p>
-	</div>
-	<div class="bottom tac">
-	<a href="javascript:;" onclick="javascript:history.go(-1);return false;" class="btn">ย้อนกลับ</a>
-	</div>
-</div>
