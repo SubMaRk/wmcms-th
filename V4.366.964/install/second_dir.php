2d583fbb7f3d9b77372c5ac66595f7e8fe8d1108
Yutthaphon Inchaiya <yutthaphon30667@gmail.com>
2021-03-25 19:58:44 +0700

    Delete

--- V4.366.964/install/second_dir.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('WMINC'))die();?>
-<div class="main cc">
-	<div class="success_tip error_tip" style="margin-bottom: 30px;">
-	<p>ยังไม่รองรับการติดตั้งในไดเรกทอรี่รองในตอนนี้ โปรดติดตั้งในไดเรกทอรี่หลัก</p>
-	</div>
-	<div class="bottom tac">
-	<a href="javascript:;" onclick="javascript:history.go(-1);return false;" class="btn">ย้อนกลับ</a>
-	</div>
-</div>
--- V4.366.964/install/sql.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('WMINC'))die();?>
-<div class="main cc">
-	<div class="success_tip error_tip" style="margin-bottom: 30px;">
-	<p>ขออภัย! แพ็คเก็ตการติดตั้งไม่มีไฟล์ .sql อยู่ จึงไม่สามารถติดตั้งได้</p>
-	</div>
-	<div class="bottom tac">
-	<a href="javascript:;" onclick="javascript:history.go(-1);return false;" class="btn">ย้อนกลับ</a>
-	</div>
-</div>
